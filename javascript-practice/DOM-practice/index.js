570f9757daf3aec94ccdda53eb69015cff284b77
Faith Zhang <fz2*@rice.edu>
2024-08-02 00:13:26 -0500

    dom exercises 1-3

--- javascript-practice/DOM-practice/index.js
+++ javascript-practice/DOM-practice/index.js
@@ -9,7 +9,32 @@ container.appendChild(content);
 
 //exercises 1-3:
 const red= document.createElement('p');
+red.style.color= 'red';
 red.textContent= "Hey I'm red!";
+container.appendChild(red);
+
+
+const blueHeading= document.createElement('h3');
+blueHeading.style.color= 'blue';
+blueHeading.textContent= "I'm a blue h3!";
+container.appendChild(blueHeading)
+
+
+const newDiv= document.createElement('div');
+newDiv.setAttribute('style', 'background-color: pink; border: 2px black;');
+
+
+const newHeading= document.createElement('h1');
+newHeading.textContent = "I'm in a div hehhe";
+newDiv.appendChild(newHeading);
+
+const newP= document.createElement('p');
+newP.textContent= "ME TOOOO";
+newDiv.appendChild(newP)
+
+
+container.appendChild(newDiv)
+
 
 
 
