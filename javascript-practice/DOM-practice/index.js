dac1c3ab5de69a74a70ab35e41feef227fcb9dd9
Faith Zhang <fz2*@rice.edu>
2024-08-01 23:45:17 -0500

    dom practice exercises

---
+++ javascript-practice/DOM-practice/index.js
@@ -0,0 +1,17 @@
+const container= document.querySelector('#container');
+
+const content= document.createElement("div");
+content.classList.add("content");
+content.textContent= "This is the glorious text-content!";
+
+container.appendChild(content);
+
+
+//exercises 1-3:
+const red= document.createElement('p');
+red.textContent= "Hey I'm red!";
+
+
+
+
+console.log('run finished')
